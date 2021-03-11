1657bfda1145ccea25810f47176e7c29466c49b0
arthurvergacas <arthurvdm@gmail.com>
2021-03-11 15:09:30 -0300

    fix site domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	env: {
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		SITE_DOMAIN:
-			process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.LOCAL_SITE_DOMAIN,
+			process.env.LOCAL_SITE_DOMAIN ?? process.env.NEXT_PUBLIC_VERCEL_URL,
 	},
 };
